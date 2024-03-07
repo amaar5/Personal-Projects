db9577e040a975995dd5404002763012e439b769
amaar5 <53356372+amaar5@users.noreply.github.com>
2024-03-07 16:54:02 -0500

    Update Sort of Hangman.py

--- hangman (not finished)/Sort of Hangman.py
+++ hangman (not finished)/Sort of Hangman.py
@@ -18,27 +18,27 @@ def hangman():
     guessed_letters = []
     attempts = 6
 
-    print("Welcome to Hangman!")
-    print("Guess the word. You have 6 attempts.")
+    print("Hangman")
+    print("Guess the word but you only have 6 tries")
 
     while attempts > 0:
         print("\nWord:", display_word(word, guessed_letters))
         guess = input("Enter a letter: ").lower()
 
         if guess in guessed_letters:
-            print("You've already guessed that letter.")
+            print("You've already guessed that letter")
         elif guess in word:
-            print("Correct!")
+            print("Correct")
             guessed_letters.append(guess)
             if all(letter in guessed_letters for letter in word):
-                print("Congratulations! You guessed the word:", word)
+                print("That is the correct word:", word)
                 return
         else:
-            print("Incorrect!")
+            print("WRONG")
             attempts -= 1
             print("Attempts left:", attempts)
             if attempts == 0:
-                print("Game over! The word was:", word)
+                print("You lost...The word was:", word)
                 return
 
 if __name__ == "__main__":
