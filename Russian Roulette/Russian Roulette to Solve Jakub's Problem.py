8311a423ea13e60b22bdb53259ffd950c0733a52
amaar5 <53356372+amaar5@users.noreply.github.com>
2024-03-07 16:51:07 -0500

    Update Russian Roulette to Solve Jakub's Problem.py

--- Russian Roulette/Russian Roulette to Solve Jakub's Problem.py
+++ Russian Roulette/Russian Roulette to Solve Jakub's Problem.py
@@ -2,25 +2,25 @@ import random
 import time
 
 def russian_roulette():
-    print("Welcome to Russian Roulette!")
-    print("You have a revolver with 6 chambers.")
-    print("One of the chambers is loaded. Let's see if you're lucky!")
+    print("Welcome to Russian Roulette Jakub")
+    print("You have a revolver with 6 rounds you know how this goes tho")
+    print("One of the chambers is loaded. Don't Like Fail")
 
     chambers = [False] * 6
     chambers[random.randint(0, 5)] = True
 
     for i in range(6):
-        print("Spinning the chamber...")
+        print("Spinning the chamber")
         time.sleep(1)
         print("Click!")
         time.sleep(1)
         if chambers[i]:
-            print("Bang! You're dead!")
+            print("BoP")
             return
         else:
-            print("Phew! You survived!")
+            print("YOU ACTUALLY WON")
 
-    print("Congratulations! You survived all 6 rounds!")
+    print("Good job jakub")
 
 if __name__ == "__main__":
     russian_roulette()
